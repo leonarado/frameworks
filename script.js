9527807fb9b233605e720dda8317e492cde9e7b4
leonarado <61057882+leonarado@users.noreply.github.com>
2020-02-20 08:14:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ console.log("Escutando a porta 3000");//Esse é só para ver a porta que estamos
 //funão do res = dar a resposta ao usuário com base na req
 
 /*
+É necessário o node para executar esse passo a passo
 Para executar e testar um código, primeiro devemos criar e salvar um arquivo JS (Ctrl+S; Nome do arquivo: nome.js; Tipo do arquivo: Todos o arquivos) e por nosso código nele, no nosso caso, já temos ele explicado acima.
 Após termos nosso código no IDE e o salvar, apertamos as teclas Windows+R, ou usamos a barra de pesquisa do Windows, agora, pesquisamos por 'cmd' e damos enter.
 Dentro do cmd, vamos para a pasta onde está salvo nosso arquivo JS, usando o comando "cd [diretório]", onde preenchemos esse local [diretório] com a pasta desejada, no caso dessa máquina foi C:\Users\aluno\Documents\Framework\aula b.
